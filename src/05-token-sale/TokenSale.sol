b2ffdb41f02fc3602231030813f5b8b0c9eb2d7a
DevDacian <114125154+devdacian@users.noreply.github.com>
2023-12-01 20:34:00 +0900

    remove console import

--- src/05-token-sale/TokenSale.sol
+++ src/05-token-sale/TokenSale.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.23;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "forge-std/console.sol";
-
 //
 // This contract is a simplified version of a real contract which
 // was audited by Cyfrin in a private audit and contained the same bugs.
