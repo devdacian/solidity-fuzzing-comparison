52382958a8b4275a12a979a5aaba7a546bb77015
DevDacian <114125154+devdacian@users.noreply.github.com>
2024-03-14 03:05:18 +0900

    spelling fix

--- test/06-rarely-false/RarelyFalseBasicFoundry.t.sol
+++ test/06-rarely-false/RarelyFalseBasicFoundry.t.sol
@@ -6,7 +6,7 @@ import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
 import {Test} from "forge-std/Test.sol";
 
 // run from base project directory with:
-// orge test --match-contract RarelyFalseBasicFoundry --fuzz-runs 2000000
+// forge test --match-contract RarelyFalseBasicFoundry --fuzz-runs 2000000
 //
 // get coverage report ( can be imported into https://lcov-viewer.netlify.app/ )
 // forge coverage --report lcov --report-file test/06-rarely-false/coverage-foundry-basic.lcov --match-contract RarelyFalseBasicFoundry
