83e1f1b9b7901bba152a6ffd76f8640cb56dca9a
DevDacian <114125154+devdacian@users.noreply.github.com>
2024-03-14 01:49:04 +0900

    refactored byte battle challenge using chimera to reduce code duplication

--- test/07-byte-battle/ByteBattleBasicFoundry.t.sol
+++ test/07-byte-battle/ByteBattleBasicFoundry.t.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "forge-std/Test.sol";
+import {ByteBattleTargetFunctions} from "./ByteBattleTargetFunctions.sol";
+import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
+import {Test} from "forge-std/Test.sol";
 
 // run from base project directory with:
 // forge test --match-contract ByteBattleBasicFoundry
@@ -9,15 +11,6 @@ import "forge-std/Test.sol";
 // get coverage report ( can be imported into https://lcov-viewer.netlify.app/ )
 // forge coverage --report lcov --report-file test/07-byte-battle/coverage-foundry-basic.lcov --match-contract ByteBattleBasicFoundry
 
-contract ByteBattleBasicFoundry is Test {
+contract ByteBattleBasicFoundry is Test, ByteBattleTargetFunctions, FoundryAsserts {
 
-    // Foundry can break it
-    function testFuzz(bytes32 a, bytes32 b) public pure {
-        vm.assume(a != b);
-        assert(_convertIt(a) != _convertIt(b));
-    }
-
-    function _convertIt(bytes32 b) private pure returns (uint96) {
-        return uint96(uint256(b) >> 160);
-    }
 }
---
+++ test/07-byte-battle/ByteBattleTargetFunctions.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Asserts} from "@chimera/Asserts.sol";
+
+// target functions to test
+abstract contract ByteBattleTargetFunctions is Asserts {
+
+    // fuzzers call this function
+    function test_ByteBattle(bytes32 a, bytes32 b) external {
+        // input precondition
+        precondition(a != b);
+
+        // assertion to break
+        t(_convertIt(a) != _convertIt(b), "Different inputs should not convert to the same value");
+    }
+
+    // actual implementation to test
+    function _convertIt(bytes32 b) private pure returns (uint96) {
+        return uint96(uint256(b) >> 160);
+    }
+}
